0fa31e4e9d7dc43b272e9d827d2e06d770c58198
popo2381 <popo2381@icloud.com>
2025-12-18 23:35:12 +0900

    step3

--- src/com/example/commerce/Category.java
+++ src/com/example/commerce/Category.java
@@ -1,4 +1,25 @@
 package com.example.commerce;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Category {
+    private String name;
+    private List<Product> products = new ArrayList<>();
+
+    public Category(String name) {
+        this.name = name;
+    }
+    // 카테고리 이름 반환
+    public String getName() {
+        return name;
+    }
+    // 상품 추가
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+    // 카테고리 내 상품 목록 반환
+    public List<Product> getProducts() {
+        return products;
+    }
 }
