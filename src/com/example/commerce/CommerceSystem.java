c74ea6443d84bf881a8094790fcae4a14bd92b4c
popo2381 <popo2381@icloud.com>
2025-12-18 12:13:12 +0900

    step2

--- src/com/example/commerce/CommerceSystem.java
+++ src/com/example/commerce/CommerceSystem.java
@@ -1,18 +1,46 @@
 package com.example.commerce;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class CommerceSystem {
     private List<Product> products;
+    private Scanner scanner = new Scanner(System.in);
 
     public CommerceSystem(List<Product> products) {
         this.products = products;
     }
-
-    public List<Product> getproducts() {
-        return products;
+    public void start() {
+        printPlatform();
+        handleInput();
     }
-    public void addProduct(Product product) {
-        products.add(product);
+    private void printPlatform() {
+        System.out.println("[ 실시간 커머스 플랫폼   -   전자제품 ]");
+        int i = 1;
+        for (Product p : products) {
+            System.out.println(
+                    String.format(
+                            "%d. %-15s | %,9d원 | %s",
+                            i,
+                            p.getName(),
+                            p.getPrice(),
+                            p.getDescription()
+                    )
+            );
+            i++;
+        }
+        System.out.printf(
+                "%d. %-15s | %s%n",
+                0,
+                "exit",
+                "프로그램 종료"
+        );
+    }
+    private void handleInput() {
+        System.out.print("Please enter a number : ");
+        int input = scanner.nextInt();
+        if (input == 0) {
+            System.out.println("커머스 플랫폼을 종료합니다.");
+        }
     }
 }
--- src/com/example/commerce/Main.java
+++ src/com/example/commerce/Main.java
@@ -12,33 +12,7 @@ public class Main {
         products.add(new Product("iPhone 16", "Apple의 최신 스마트폰", 1350000, 10));
         products.add(new Product("MacBook Pro", "M3 칩셋이 탑재된 노트북", 2400000, 10));
         products.add(new Product("AirPods Pro", "노이즈 캔슬링 무선 이어폰", 350000, 10));
-        commercePlatform(sc, products);
-    }
-    public static void commercePlatform(Scanner sc, List<Product> products) {
-        System.out.println("[ 실시간 커머스 플랫폼   -   전자제품 ]");
-        int i = 1;
-        for (Product p : products) {
-            System.out.println(
-                    String.format(
-                            "%d. %-15s | %,9d원 | %s",
-                            i,
-                            p.getName(),
-                            p.getPrice(),
-                            p.getDescription()
-                    )
-            );
-            i++;
-        }
-        System.out.printf(
-                "%d. %-15s | %s%n",
-                0,
-                "exit",
-                "프로그램 종료"
-        );
-        System.out.print("Please enter a number : ");
-        int input = sc.nextInt();
-        if (input == 0) {
-            System.out.println("커머스 플랫폼을 종료합니다.");
-        }
+        CommerceSystem system = new CommerceSystem(products);
+        system.start();
     }
 }
