e7300aeba06d192ffcca3f6059bf37f4d617a9e3
popo2381 <popo2381@icloud.com>
2025-12-18 11:03:59 +0900

    step1

--- src/com/example/commerce/Product.java
+++ src/com/example/commerce/Product.java
@@ -1,4 +1,27 @@
 package com.example.commerce;
 
 public class Product {
+    private String name;
+    private String description;
+    private int price;
+    private int quantity;
+
+    public Product(String name, String description, int price, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
 }
